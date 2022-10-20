2bd8b5f421b58171831c5c82bf790c5023d03560
Georgie Conway <dopestantelope@gmail.com>
2022-10-20 12:45:30 +0100

    added error pages

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const morgan = require('morgan')
 const exphbs = require('express-handlebars')
 const passport = require('passport')
 const session = require('express-session')
+const MongoStore = require('connect-mongo')
 const connectDB = require('./config/db')
 
 //Load config file
@@ -32,7 +33,9 @@ app.use(session({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: false,
-  cookie: {secure: false}
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGO_URI
+  })
 }))
 
 
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router()
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
+const Story = require('../models/Story')
+
 //@desc   Login/Landing Page
 //routes  GET /
-
 router.get('/', ensureGuest, (req, res) => {
   res.render('login', {
     layout: 'login',
@@ -13,8 +14,17 @@ router.get('/', ensureGuest, (req, res) => {
 
 //@desc   Dashboard
 //routes  GET /dashboard
-router.get('/dashboard', ensureAuth, (req, res) => {
-  res.render('dashboard')
+router.get('/dashboard', ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({user: req.user.id}).lean() //returns a mongoose object, lean strips excess and leaves you with clean json
+    res.render('dashboard', {
+      name: req.user.firstName,
+      stories
+    })
+  } catch(err) {
+    console.error(err)
+    res.render('error/500')
+  }
 })
 
 
