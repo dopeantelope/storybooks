3cd59445336ee90ed2cabff55fbcbd79f7c54ff5
Georgie Conway <dopestantelope@gmail.com>
2022-10-21 18:30:31 +0100

    implemented view user stories

--- routes/stories.js
+++ routes/stories.js
@@ -40,6 +40,29 @@ router.get('/', ensureAuth, async (req, res) => {
   }
 })
 
+
+//@desc   Show single story
+//routes  GET /stories/:id
+router.get('/:id', ensureAuth, async(req, res) => {
+  try {
+    let story = await Story.findById(req.params.id)
+      .populate('user')
+      .lean()
+
+    if(!story) {
+      return res.render('error/404')
+    }
+
+    res.render('stories/show', {
+      story
+    })
+  } catch(err) {
+      console.error(err)
+      res.redirect('error/500')
+  }
+})
+
+
 //@desc   Show edit page
 //routes  GET /stories/edit/:id
 router.get('/edit/:id', ensureAuth, async(req, res) => {
@@ -64,7 +87,7 @@ router.get('/edit/:id', ensureAuth, async(req, res) => {
 //@desc Update Story
 //@Route PUT /stories/:id
 router.put('/:id', ensureAuth, async (req,res) => {
-  try{
+  try {
     let story = await Story.findById(req.params.id).lean()
 
     if(!story) {
@@ -87,5 +110,36 @@ router.put('/:id', ensureAuth, async (req,res) => {
   }
 })
 
+//@desc   Delete story
+//routes  DELETE /stories/add
+router.delete('/:id', ensureAuth, async (req, res) => {
+  try {
+    await Story.remove({_id: req.params.id})
+    res.redirect('/dashboard')
+  } catch(err) {
+      console.error(err)
+      return res.render('error/500')
+    }
+})
+
+//@desc   User stories
+//routes  GET /stories/add
+router.get('/user/:userId', ensureAuth, async(req, res) => {
+  try {
+    const stories = await Story.find({
+      user: req.params.userId,
+      status: 'public'
+    })
+    .populate('user')
+    .lean()
+
+    res.render('stories/index', {
+      stories
+    })
+  } catch(error) {
+      console.error(err)
+      res.render('error/500')
+  }
+})
 
 module.exports = router
